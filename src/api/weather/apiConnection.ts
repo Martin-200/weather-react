f82977f5ff8deb976c55969908cf839b7365f0c6
Martin-200 <martin.piolat69@gmail.com>
2024-02-20 14:37:08 +0100

    Fix: hide api key

--- src/api/weather/apiConnection.ts
+++ src/api/weather/apiConnection.ts
@@ -1,6 +1,6 @@
 export const api = {
     base: 'https://api.openweathermap.org/',
-    key: '7a4a05041d18b026aa29508b0cf9efcd', // 7a4a05041d18b026aa29508b0cf9efcd 6aadf54f59f9c2d3a4c3973628015400
+    key: import.meta.env.VITE_OPENWEATHERMAP_API_KEY as string,
 }
 
 export const apiCall = (call: string, lang: string = 'fr') => {
