104f0b82ef3de4cd0c427ea8890e28cb4dd148fe
Martin-200 <martin.piolat69@gmail.com>
2024-02-20 19:14:07 +0100

    React router refresh problem fixed #2

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 import { I18nextProvider } from 'react-i18next'
 import i18n from '../i18n.ts'
 
-const isProduction = process.env.NODE_ENV === 'production'
+// const isProduction = process.env.NODE_ENV === 'production'
 
 import './assets/scss/tailwind.scss'
 import './assets/scss/main.scss'
@@ -15,7 +15,8 @@ const container = document.getElementById('root')
 const root = ReactDOM.createRoot(container!)
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename={isProduction ? '/weather-react' : '/'}>
+        <BrowserRouter basename='/'>
+        {/* <BrowserRouter basename={isProduction ? '/weather-react' : '/'}> */}
             <I18nextProvider i18n={i18n}>
                 <App />
             </I18nextProvider>
