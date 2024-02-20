2e3efd3d7c36c04af7d753ad94b51fc8367e4310
Martin-200 <martin.piolat69@gmail.com>
2024-02-20 17:59:23 +0100

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/weather-react/',
     plugins: [
         svgr({
             include: "**/*.svg?react",
